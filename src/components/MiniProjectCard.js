470972053d4cecba62411ca8264aac1803e1a974
Tuan Anh Hoang <anhtuan.ash@gmail.com>
2022-05-23 19:08:19 +0700

    Update Mini Projects

--- src/components/MiniProjectCard.js
+++ src/components/MiniProjectCard.js
@@ -1,17 +1,25 @@
 import React from "react";
+import { useWasViewed } from "../hooks/useWasViewed";
 import Button from "./Button";
 
 export default function MiniProjectCard({ project }) {
   const { name, website, preview } = project;
+  const DOMAIN = "https://projects.anhtuan.info/projects/";
+
+  const { setRef, wasViewed } = useWasViewed();
+  const animation = wasViewed ? "animate-slide-in-bottom" : "";
   return (
-    <div className=" rounded-md overflow-hidden p-1 bg-[#2f3744] relative group">
+    <div
+      className={` rounded-md overflow-hidden p-1 bg-[#2f3744] relative group ${animation}`}
+      ref={setRef}
+    >
       <img
-        src={preview}
+        src={preview !== "" ? DOMAIN + preview : null}
         alt={name}
         className="rounded-md group-hover:opacity-10"
       />
       <div className=" flex items-center justify-center absolute top-0 right-0 w-full h-full invisible group-hover:visible">
-        <a href={website} target="_blank" rel="noopener noreferrer">
+        <a href={DOMAIN + website} target="_blank" rel="noopener noreferrer">
           <Button content="Live Demo" />
         </a>
       </div>
--- src/sections/MainProjects.js
+++ src/sections/MainProjects.js
@@ -1,8 +1,10 @@
+import axios from "axios";
 import React from "react";
+import { useState, useEffect } from "react";
 import Heading from "../components/Heading";
 import MainProjectCard from "../components/MainProjectCard";
 import MiniProjectCard from "../components/MiniProjectCard";
-import { featured, miniProjects } from "../utils/config";
+import { featured } from "../utils/config";
 
 export default function MainProjects(props) {
   return (
@@ -12,13 +14,47 @@ export default function MainProjects(props) {
         {featured.map((project) => (
           <MainProjectCard project={project} key={project.id} />
         ))}
-        <Heading text="Mini Projects" />
-        <div className=" grid gap-4 grid-cols-1 md:grid-cols-2">
-          {miniProjects.map((project) => (
-            <MiniProjectCard project={project} key={project.name} />
-          ))}
-        </div>
+        <MiniProjects />
       </div>
     </div>
   );
 }
+
+const MiniProjects = () => {
+  const [projects, setProjects] = useState([
+    {
+      _id: "",
+      name: "",
+      website: "",
+      preview: "",
+    },
+  ]);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        axios
+          .get("https://projects.anhtuan.info/api/mini-projects")
+          .then((res) => res.data.projects.reverse())
+          .then((res) => setProjects(res));
+      } catch (error) {
+        console.log(error);
+      }
+    })();
+  }, []);
+
+  return (
+    <>
+      {projects[0].name === "" ? null : (
+        <>
+          <Heading text="Mini Projects" />
+          <div className=" grid gap-4 grid-cols-1 md:grid-cols-2 xl:grid-cols-3">
+            {projects.map((project) => (
+              <MiniProjectCard project={project} key={project._id} />
+            ))}
+          </div>
+        </>
+      )}
+    </>
+  );
+};
