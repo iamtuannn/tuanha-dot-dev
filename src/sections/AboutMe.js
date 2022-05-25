0e22a12b0693209c45bfc875fb568095996e5dc0
Tuan Anh Hoang <anhtuan.ash@gmail.com>
2022-05-26 05:12:05 +0700

    hidden certification-cybersoft.jpg

--- src/sections/AboutMe.js
+++ src/sections/AboutMe.js
@@ -1,18 +1,18 @@
-import React, { useState } from "react";
+import React from "react";
 import Heading from "../components/Heading";
-import cybersoft from "../assets/certification-cybersoft.jpg";
+// import cybersoft from "../assets/certification-cybersoft.jpg";
 import { facts } from "../utils/config";
 import Highlight from "../components/Highlight";
 
 export default function AboutMe(props) {
-  const [blur, setBlur] = useState(true);
+  // const [blur, setBlur] = useState(true);
 
   return (
     <div className=" p-auto flex-center" id={props.id || ""}>
       <div className=" box">
         <Heading text="About Me" />
         <div className=" flex w-full bg-box rounded-2xl shadow-primary overflow-hidden">
-          <div
+          {/* <div
             className=" hidden xl:block min-w-[240px] bg-cover bg-no-repeat bg-center cursor-pointer min-h-[320px] overflow-hidden duration-[2s] ease-in bg-cybersoft"
             style={{
               backgroundImage: `url(${cybersoft})`,
@@ -20,7 +20,7 @@ export default function AboutMe(props) {
             }}
             onMouseEnter={() => setBlur(false)}
             onMouseLeave={() => setBlur(true)}
-          ></div>
+          ></div> */}
           <div className=" p-4 md:p-8">
             {facts.map((fact, i) => (
               <Highlight key={i} highlight={fact} />
